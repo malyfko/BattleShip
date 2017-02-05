3ddf1efe7a03dfe6077213a099de5ebc1b63ecad
Tetiana Platonova <malyfko@gmail.com>
2017-02-05 16:44:24 +0100

    Implement render function in Cell

--- scripts/Cell.jsx
+++ scripts/Cell.jsx
@@ -5,15 +5,25 @@ class Cell extends React.Component {
     super(props);
   }
   render () {
-    return (
-      <div className="cell">O</div>
+    if (this.props.fieldType == 'user')
+      return (
+        <div className="cell">U</div>
+      )
+    else if (this.props.attempted) //this.props.fieldType == 'computer'
+      return (
+        <div className="cell">X</div>
+      )
+    else return (
+      <div className="cell clickable" onClick={this.props.cellAttempt}>O</div>
     );
   }
 }
 
 Cell.propTypes = {
   coveredWithAShip: React.PropTypes.bool.isRequired,
-  attempted: React.PropTypes.bool.isRequired
+  attempted: React.PropTypes.bool.isRequired,
+  cellAttempt: React.PropTypes.func.isRequired,
+  fieldType : React.PropTypes.oneOf(['user','computer']).isRequired
 };
 
 export default Cell;
