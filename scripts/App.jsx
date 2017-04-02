b88fdf882c904fb8bd4c3f62e5b34443c2596609
Tetiana Platonova <malyfko@gmail.com>
2017-04-02 10:42:40 +0200

    Add shooting player recognition

--- scripts/App.jsx
+++ scripts/App.jsx
@@ -7,21 +7,52 @@ class App extends React.Component{
   constructor() {
     super();
     this.state = {
-      startGame: false
+      startGame: false,
+      shootingTurn : null
     };
     this.startGame = this.startGame.bind(this);
+    this.chooseFirstShooter = this.chooseFirstShooter.bind(this);
+    this.switchPlayer = this.switchPlayer.bind(this);
   }
 
   startGame() {
 
   }
 
+  chooseFirstShooter() {
+    let players = [`user`, `computer`];
+    this.setState({
+      shootingTurn: players[Math.round(Math.random())],
+      startGame: true
+    })
+  }
+
+  switchPlayer() {
+    console.log(`switch player called`);
+    console.log(`this.state.shootingTurn in App`, this.state.shootingTurn);
+    if (this.state.shootingTurn === `user`)
+      this.setState({
+        shootingTurn: `computer`
+      });
+    else
+      this.setState({
+        shootingTurn: `user`
+      });
+  }
 
   render() {
     return (
-      <div className="wrapper">
-        <GameField fieldType={`user`}/>
-        <GameField fieldType={`computer`}/>
+      <div id="game">
+        <div className="wrapper">
+          <GameField fieldType={`user`}
+                     shootingTurn={this.state.shootingTurn}
+                     switchPlayer={this.switchPlayer}/>
+          <GameField fieldType={`computer`}
+                     shootingTurn={this.state.shootingTurn}
+                     switchPlayer={this.switchPlayer}/>
+        </div>
+        {this.state.startGame ? null :
+          <button onClick={this.chooseFirstShooter}>Start game</button>}
       </div>
     );
   }
