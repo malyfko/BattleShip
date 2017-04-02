db6321bcd2216f3ca35783e3a9271fb40e3b8bc0
Tetiana Platonova <malyfko@gmail.com>
2017-04-02 10:36:14 +0200

    Change ship display

--- scripts/Cell.jsx
+++ scripts/Cell.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
+import Ship from './Ship';
 
 class Cell extends React.Component {
   constructor (props) {
     super(props);
   }
   render () {
-    if (this.props.fieldType == 'user')
+    if (this.props.attempted) {
+      if (this.props.correspondingShip)
+        return (
+          <div className="cell">X</div>
+        )
+      else
+        return (
+          <div className="cell">&middot;</div>
+        )
+    }
+    else if (this.props.fieldType == 'user')
       return (
-        <div className="cell">U</div>
-      )
-    else if (this.props.attempted) //this.props.fieldType == 'computer'
-      return (
-        <div className="cell">X</div>
-      )
-    else return (
-      <div className="cell clickable" onClick={this.props.cellAttempt}>O</div>
-    );
+        <div className="cell">&nbsp;</div>
+      );
+      else return (
+        <div className="cell clickable" onClick={this.props.cellAttempt}>&nbsp;</div>
+      );
   }
 }
 
 Cell.propTypes = {
-  coveredWithAShip: React.PropTypes.bool.isRequired,
+  correspondingShip: React.PropTypes.instanceOf(Ship),
   attempted: React.PropTypes.bool.isRequired,
   cellAttempt: React.PropTypes.func.isRequired,
   fieldType : React.PropTypes.oneOf(['user','computer']).isRequired
